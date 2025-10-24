294862120600d7c83b22235d6808c322db47d71a
CUIUDO061 <bruno.estefani.souza@escola.pr.gov.br>
2025-10-24 10:36:27 -0300

    .

--- app.js
+++ app.js
@@ -3,3 +3,12 @@ function criaCartao(categoria,pergunta,resposta){
     let cartao=document.createElement('article')
     cartao.className='cartao'
 }
+cartao.innerHTML='
+<div class="cartao_conteudo">
+<h3>${categoria}</h3>
+<div class="cartao_conteudo_pergunta">
+    </div>
+    <div class="cartao_conteudo_resposta">
+        <p>${resposta}</p>
+        </div>
+        </div>
